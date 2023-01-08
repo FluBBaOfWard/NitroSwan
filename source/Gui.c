92702153980dfae2bd9457673e6077a3432c7624
Fredrik Ahlström <fredrikahlstrom75@gmail.com>
2023-01-08 11:27:43 +0100

    Updated cpu core with optimized v30ModRmRm access.

--- source/Gui.c
+++ source/Gui.c
@@ -12,7 +12,7 @@
 #include "ARMV30MZ/Version.h"
 #include "Sphinx/Version.h"
 
-#define EMUVERSION "V0.4.2 2023-01-05"
+#define EMUVERSION "V0.4.2 2023-01-08"
 
 #define ALLOW_SPEED_HACKS	(1<<17)
 #define ENABLE_HEADPHONES	(1<<18)
